0e232239e237337a020f6043ffa6e60db9903d25
RyQuest <rajkumaryadav@questglt.com>
2022-08-26 15:42:17 +0530

    vfbgf

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -9,44 +9,6 @@ $partner_comission = 10;
 
 
 
-function sendNotification($mypost)
-    {
-        // $firebaseToken = User::whereNotNull('device_token')->pluck('device_token')->all();
-        $firebaseToken = $mypost['firebaseToken'];
-            
-        $SERVER_API_KEY = env('FCM_SERVER_KEY');
-    
-        $data = [
-            "registration_ids" => $firebaseToken,
-            "notification" => [
-                "title" => $mypost['title'],
-                "body" => $mypost['body'],  
-            ],
-            "data" => isset($mypost['data']) ? $mypost['data'] : null,
-        ];
-		
-        $dataString = json_encode($data);
- // return $dataString;     
-        $headers = [
-            'Authorization: key=' . $SERVER_API_KEY,
-            'Content-Type: application/json',
-        ];
-      
-        $ch = curl_init();
-        
-        curl_setopt($ch, CURLOPT_URL, 'https://fcm.googleapis.com/fcm/send');
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $dataString);
-                 
-        $response = curl_exec($ch);
-        return $response;
-        // return back()->with('success', 'Notification send successfully.');
-    }
-
-
 function vle_comission($amount){
     $comm = ($amount * $vle_comission) / 100;
     return $comm;
