c9d4248b654258fd5d912eac5c86cf089a9c5ddb
RyQuest <rajkumaryadav@questglt.com>
2022-08-27 07:55:05 +0530

    xvdbg

--- app/Http/Controllers/api/AppointmentController.php
+++ app/Http/Controllers/api/AppointmentController.php
@@ -431,7 +431,7 @@ class AppointmentController extends Controller
 
         $wallet=UserWallet::where(['user_id'=>$request->user_id,'user_role'=>'vle'])->first();
 
-        $todayAppointment=Appointment::where(['added_by'=>$request->user_id,'created_at'=>Carbon::today()])->count();
+        $todayAppointment=Appointment::where('added_by',$request->user_id)->whereDate('created_at',Carbon::today())->count();
 
         $balance=$wallet->amount;
         
