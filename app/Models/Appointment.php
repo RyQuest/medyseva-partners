0a86bd343d66de96b4a2034953b71da3a9c671df
RyQuest <rajkumaryadav@questglt.com>
2022-09-02 14:06:35 +0530

    bgfngh

--- app/Models/Appointment.php
+++ app/Models/Appointment.php
@@ -11,4 +11,30 @@ class Appointment extends Model
 
     protected $guarded = ['id'];
 
+    public function patients(){
+        return $this->hasMany(Patients::class, 'id', 'patient_id');
+    }
+
+    public function patient(){
+        return $this->hasOne(Patients::class, 'id', 'patient_id');
+    }
+
+    public function chamber(){
+        return $this->belongsTo(Chamber::class, 'chamber_id', 'uid');
+    }
+
+    public function doctor(){
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
+    public function payment_user(){
+        return $this->hasOne(PaymentUser::class, 'appointment_id', 'id');
+    }
+
+    public function invoice(){
+        return $this->hasOne(Invoices::class, 'appointment_id', 'id');
+    }
+   
+
+    
 }
